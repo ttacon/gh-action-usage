1296364c3a9ccaeaafcd7c4bafcae17d9ad9d3b3
Trey Tacon <ttacon@gmail.com>
2022-12-09 22:32:16 -0500

    chore: cleanup WIP code

--- go.mod
+++ go.mod
@@ -4,5 +4,9 @@ go 1.16
 
 require (
 	github.com/google/go-github/v48 v48.1.0 // indirect
+	github.com/schollz/progressbar/v3 v3.12.2 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/oauth2 v0.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -12,32 +12,52 @@ import (
 	"time"
 
 	"github.com/google/go-github/v48/github"
+	"go.uber.org/zap"
 	"golang.org/x/oauth2"
 )
 
 var (
-	apiToken = flag.String("api-token", "", "GitHub API token")
-	orgName  = flag.String("org", "", "GitHub org name")
+	apiToken     = flag.String("api-token", "", "GitHub API token")
+	orgName      = flag.String("org", "", "GitHub org name")
+	numRepos     = flag.Int("num-repos", 25, "Number of repos to inspect")
+	privateRepos = flag.Bool("private", true, "Private repos or public")
+	isVerbose    = flag.Bool("is-verbose", false, "Run in verbose mode")
 )
 
 // repo -> workflow ID -> run -> usage
 type WorkflowRunUsage struct {
-	runID int64 
+	runID     int64
 	runTiming int64
 }
 
 type WorkflowUsage struct {
-	id int64
+	id   int64
 	name string
 	runs []*WorkflowRunUsage
 }
 
 func main() {
 	flag.Parse()
-	fmt.Println("booting up")
+
+	loggerConfig := zap.NewProductionConfig()
+	loggerConfig.Encoding = "console"
+	loggerConfig.DisableStacktrace = true
+	loggerConfig.DisableCaller = true
+	if *isVerbose {
+		loggerConfig.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	}
+
+	logger, err := loggerConfig.Build()
+	if err != nil {
+		fmt.Println("failed to create logger, err: ", err)
+		os.Exit(1)
+	}
+	lgr := logger.Sugar()
+	lgr.Infoln("booting up client for analysis")
 
 	var data = make(map[string]map[int64]WorkflowUsage)
 
+	lgr.Debugln("constructing GitHub client")
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: *apiToken},
@@ -46,135 +66,127 @@ func main() {
 
 	client := github.NewClient(tc)
 
+	repoType := "private"
+	if len(*apiToken) == 0 {
+		lgr.Debugln("no token provided, running in public mode - analysis is limited by GitHub rate limit")
+		client = github.NewClient(nil)
+	}
+
+	if !*privateRepos {
+		lgr.Infoln("only looking at public repositories")
+		repoType = "public"
+	}
+
 	repos, _, err := client.Repositories.ListByOrg(ctx, *orgName, &github.RepositoryListByOrgOptions{
-		Type: "private",
-		Sort: "pushed",
+		Type:      repoType,
+		Sort:      "pushed",
 		Direction: "desc",
 		ListOptions: github.ListOptions{
-			PerPage: 50,
+			PerPage: *numRepos,
 		},
 	})
 	if err != nil {
 		fmt.Println("failed to list repos, err:", err)
 		os.Exit(1)
 	}
+	lgr.Infof("identified %d repositories to inspect", len(repos))
+
+	date := time.Now().Add(-1 * time.Hour * 24 * 30).Format("2006-01-02")
+	lgr.Infof("pulling workflow runs since: %q", date)
 
 	for repoII, repo := range repos {
 		repoWorkflowUsage := make(map[int64]WorkflowUsage)
 		data[*repo.Name] = repoWorkflowUsage
-		fmt.Printf("[%d/%d] checking %q\n", repoII, len(repos), *repo.Name)
+		lgr.Infof("[%d/%d] checking %q", repoII, len(repos), *repo.Name)
 
-	// list all repositories for the authenticated user
-	workflows, resp, err := client.Actions.ListWorkflows(ctx, *orgName, *repo.Name, nil)
-	if err != nil {
-		if resp.StatusCode == 404 {
-			continue
-		}
-		fmt.Println("failed to list workflows, err: ", err)
-		
-		os.Exit(1)
-	}
-
-
-	date := time.Now().Add(-1*time.Hour*24*30).Format("2006-01-02")
-	fmt.Printf("pulling workflow runs since: %q\n", date)
-
-	for _, workflow := range workflows.Workflows {
-		wfUsage := WorkflowUsage{
-			id: *workflow.ID,
-			name: *workflow.Name,
-		}
-		repoWorkflowUsage[*workflow.ID] = wfUsage
-		runs, resp, err := client.Actions.ListWorkflowRunsByID(
-			ctx,
-			*orgName,
-			*repo.Name,
-			*workflow.ID,
-			&github.ListWorkflowRunsOptions{
-				Created: fmt.Sprintf(">=%s", date),
-			},
-		)
+		// list all repositories for the authenticated user
+		workflows, resp, err := client.Actions.ListWorkflows(ctx, *orgName, *repo.Name, nil)
 		if err != nil {
 			if resp.StatusCode == 404 {
 				continue
 			}
-			fmt.Println("failed to get run, err: ", err)
-			continue
+			lgr.Error("failed to list workflows, err: ", err)
+
+			os.Exit(1)
 		}
-		for _, run := range runs.WorkflowRuns {
-			usage, resp, err := client.Actions.GetWorkflowRunUsageByID(
+
+		for _, workflow := range workflows.Workflows {
+			wfUsage := WorkflowUsage{
+				id:   *workflow.ID,
+				name: *workflow.Name,
+			}
+			repoWorkflowUsage[*workflow.ID] = wfUsage
+			runs, resp, err := client.Actions.ListWorkflowRunsByID(
 				ctx,
 				*orgName,
 				*repo.Name,
-				*run.ID,
+				*workflow.ID,
+				&github.ListWorkflowRunsOptions{
+					Created: fmt.Sprintf(">=%s", date),
+				},
 			)
 			if err != nil {
 				if resp.StatusCode == 404 {
 					continue
 				}
-				fmt.Println("failed to get run usage, err:",err)
-				continue
-			} else if usage == nil || usage.RunDurationMS == nil {
-				fmt.Println("early exit")
+				lgr.Error("failed to get run, err: ", err)
 				continue
 			}
+			for _, run := range runs.WorkflowRuns {
+				usage, resp, err := client.Actions.GetWorkflowRunUsageByID(
+					ctx,
+					*orgName,
+					*repo.Name,
+					*run.ID,
+				)
+				if err != nil {
+					if resp.StatusCode == 404 {
+						continue
+					}
+					lgr.Error("failed to get run usage, err:", err)
+					continue
+				} else if usage == nil || usage.RunDurationMS == nil {
+					continue
+				}
 
-			// dataJSON, _ := json.Marshal(usage)
-			// fmt.Println(string(dataJSON))
-			
-			// fmt.Println(wfUsage.runs)
-			var billableMS int64 
-			if billable := usage.GetBillable(); billable != nil  {
-				if platform := billable.GetMacOS(); platform != nil {
-					billableMS += platform.GetTotalMS()
+				var billableMS int64
+				if billable := usage.GetBillable(); billable != nil {
+					if platform := billable.GetMacOS(); platform != nil {
+						billableMS += platform.GetTotalMS()
+					}
+					if platform := billable.GetUbuntu(); platform != nil {
+						billableMS += platform.GetTotalMS()
+					}
+					if platform := billable.GetWindows(); platform != nil {
+						billableMS += platform.GetTotalMS()
+					}
 				}
-				if platform := billable.GetUbuntu(); platform != nil {
-					billableMS += platform.GetTotalMS()
-				}
-				if platform := billable.GetWindows(); platform != nil {
-					billableMS += platform.GetTotalMS()
-				}
+
+				wfUsage.runs = append(wfUsage.runs, &WorkflowRunUsage{
+					runID:     *run.ID,
+					runTiming: billableMS,
+				})
 			}
-
-			wfUsage.runs = append(wfUsage.runs, &WorkflowRunUsage{
-				runID: *run.ID,
-				runTiming: billableMS,
-			})
-			
-			// fmt.Println(wfUsage.runs)
+			repoWorkflowUsage[*workflow.ID] = wfUsage
 		}
-		repoWorkflowUsage[*workflow.ID] = wfUsage
-	}
 	}
 
-	// fmt.Printf("%v\n", data)
-	// raw, _ := json.MarshalIndent(data, "", "  ")
-	// fmt.Println(string(raw))
+	lgr.Info("data retrieval completed")
 
 	buf := bytes.NewBuffer(nil)
 	csvOut := csv.NewWriter(buf)
 	for repoName, usage := range data {
-		fmt.Println("----------")
-		fmt.Println(repoName)
 		for _, usge := range usage {
-			fmt.Printf("[%d] %s: ", usge.id, usge.name)
 			var runTotal int64 = 0
 			numRuns := len(usge.runs)
 			if numRuns == 0 {
-				fmt.Println("0 runs")
 				continue
-			} 
+			}
 			for _, run := range usge.runs {
 				runTotal += run.runTiming
 			}
 
-			avgRun := int(runTotal)/numRuns
-			fmt.Printf(
-				"num runs: %d, total time: %d, avg time: %d\n",
-				numRuns,
-				runTotal,
-				avgRun,
-			)
+			avgRun := int(runTotal) / numRuns
 			csvOut.Write([]string{
 				repoName,
 				strconv.FormatInt(usge.id, 10),
@@ -186,8 +198,13 @@ func main() {
 		}
 	}
 	csvOut.Flush()
-	if err := ioutil.WriteFile("gh-action-usage.csv", buf.Bytes(), os.ModePerm); err != nil {
-		fmt.Println("failed to write file, err:", err)
+
+	fileName := fmt.Sprintf("gh-action-usage-%s-%s.csv", *orgName, date)
+
+	if err := ioutil.WriteFile(fileName, buf.Bytes(), os.ModePerm); err != nil {
+		lgr.Error("failed to write file, err:", err)
 	}
+
+	lgr.Infof("data written to %q", fileName)
 
 }
